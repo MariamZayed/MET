ebe2f84cf94ab60ff0840210e9beca518a941558
Mariam Zayed <66179261+MariamZayed@users.noreply.github.com>
2025-11-13 17:20:20 +0200

    Update and rename rr.c to 4-rr.c

--- Operating System/Sections/sec6/Code - CPU Scheduling Algorithms/4-rr.c
+++ Operating System/Sections/sec6/Code - CPU Scheduling Algorithms/4-rr.c
@@ -5,7 +5,8 @@ int main() {
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int bt[n], at[n], wt[n], tat[n], rem_bt[n];
+    int at[n], bt[n], rem_bt[n], st[n], ft[n], wt[n], tat[n];
+    int started[n];
     float total_wt = 0, total_tat = 0;
 
     printf("Enter Arrival Time and Burst Time for each process:\n");
@@ -13,40 +14,48 @@ int main() {
         printf("P%d: ", i + 1);
         scanf("%d %d", &at[i], &bt[i]);
         rem_bt[i] = bt[i];
+        started[i] = 0;
     }
 
     printf("Enter Quantum Time: ");
     scanf("%d", &qt);
 
-    int t = 0;
-    while (1) {
-        int done = 1;
+    int t = 0, complete = 0;
+
+    while (complete != n) {
+        int executed = 0;
         for (int i = 0; i < n; i++) {
             if (rem_bt[i] > 0 && at[i] <= t) {
-                done = 0;
+                executed = 1;
+                if (!started[i]) {
+                    st[i] = t;
+                    started[i] = 1;
+                }
+
                 if (rem_bt[i] > qt) {
                     t += qt;
                     rem_bt[i] -= qt;
                 } else {
                     t += rem_bt[i];
-                    wt[i] = t - bt[i] - at[i];
                     rem_bt[i] = 0;
+                    ft[i] = t;
+                    tat[i] = ft[i] - at[i];
+                    wt[i] = tat[i] - bt[i];
+                    complete++;
                 }
             }
         }
-        if (done == 1)
-            break;
+        if (!executed) t++;
     }
 
     for (int i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
         total_wt += wt[i];
         total_tat += tat[i];
     }
 
-    printf("\nProcess\tAT\tBT\tWT\tTAT\n");
+    printf("\nPro\tAT\tBT\tST\tFT\tWT\tTAT\n");
     for (int i = 0; i < n; i++)
-        printf("P%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], wt[i], tat[i]);
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], st[i], ft[i], wt[i], tat[i]);
 
     printf("\nAverage Waiting Time = %.2f", total_wt / n);
     printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);

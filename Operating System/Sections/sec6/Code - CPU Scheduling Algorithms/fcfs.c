493b140b6a74fad5369c5b06ede96b6ff7962872
Mariam Zayed <66179261+MariamZayed@users.noreply.github.com>
2025-11-13 16:13:47 +0200

    Update fcfs.c

--- Operating System/Sections/sec6/Code - CPU Scheduling Algorithms/fcfs.c
+++ Operating System/Sections/sec6/Code - CPU Scheduling Algorithms/fcfs.c
@@ -5,7 +5,7 @@ int main() {
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int at[n], bt[n], wt[n], tat[n], ft[n];
+    int at[n], bt[n], st[n], ft[n], wt[n], tat[n];
     float total_wt = 0, total_tat = 0;
 
     printf("Enter Arrival Time and Burst Time for each process:\n");
@@ -18,21 +18,28 @@ int main() {
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (at[i] > at[j]) {
-                int temp = at[i]; at[i] = at[j]; at[j] = temp;
+                int temp;
+                temp = at[i]; at[i] = at[j]; at[j] = temp;
                 temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
             }
         }
     }
 
-    // Compute Finish, TAT, WT
-    ft[0] = at[0] + bt[0];
+    // Calculate Start Time and Finish Time
+    st[0] = at[0];
+    if (st[0] < at[0])
+        st[0] = at[0];
+    ft[0] = st[0] + bt[0];
+
     for (int i = 1; i < n; i++) {
-        if (at[i] > ft[i-1])
-            ft[i] = at[i] + bt[i];
+        if (at[i] > ft[i - 1])
+            st[i] = at[i];
         else
-            ft[i] = ft[i-1] + bt[i];
+            st[i] = ft[i - 1];
+        ft[i] = st[i] + bt[i];
     }
 
+    // Calculate TAT and WT
     for (int i = 0; i < n; i++) {
         tat[i] = ft[i] - at[i];
         wt[i] = tat[i] - bt[i];
@@ -40,9 +47,9 @@ int main() {
         total_tat += tat[i];
     }
 
-    printf("\nProcess\tAT\tBT\tFT\tWT\tTAT\n");
+    printf("\nPro\tAT\tBT\tST\tFT\tWT\tTAT\n");
     for (int i = 0; i < n; i++)
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ft[i], wt[i], tat[i]);
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], st[i], ft[i], wt[i], tat[i]);
 
     printf("\nAverage Waiting Time = %.2f", total_wt / n);
     printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);
